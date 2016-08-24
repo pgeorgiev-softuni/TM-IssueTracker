5e6ca1dd6c8809d2642700486571a0282769aa28
Petar Georgiev <petar@pgeorgiev.com>
2016-08-24 16:06:58 +0300

    Add project files.

---
+++ TM-IssueTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TM_IssueTracker.MvcApplication" Language="C#" %>
---
+++ TM-IssueTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TM_IssueTracker.Startup))]
+namespace TM_IssueTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
